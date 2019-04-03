04dfacb6a30a492e5a782650a14d794b9ecfd17d
Alexis Chevalier <alexis.chevalier1@gmail.com>
2019-04-03 23:47:34 +0200

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+USE_REMOTE_DOCKER_FILE=~/.useRemoteDocker
+REMOTE_DAEMON="ssh://alexis@192.168.1.20"
+
+#Defining which daemon will be used
+if [ -f $USE_REMOTE_DOCKER_FILE ]
+then
+	USE_REMOTE_DOCKER=$(<$USE_REMOTE_DOCKER_FILE)
+else
+  	USE_REMOTE_DOCKER=0
+fi
+
+#Docker opts definition
+if [ $USE_REMOTE_DOCKER == 1 ]
+then
+	echo "[Using remote docker daemon]"
+	DOCKER_OPTS="-H=$REMOTE_DAEMON"
+else
+	echo "[Using local docker daemon]"
+	DOCKER_OPTS=""
+fi
+
+_docker-compose $DOCKER_OPTS "$@"
