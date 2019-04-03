fb5ee6296aa0dc5eec7ae7ca9d29d646dc001415
Alexis Chevalier <alexis.chevalier1@gmail.com>
2019-04-03 23:46:59 +0200

    Update and rename execdocker.sh to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-EXEC_TYPE="$1"
-shift 1
-
 USE_REMOTE_DOCKER_FILE=~/.useRemoteDocker
 REMOTE_DAEMON="ssh://user@remotehost"
 
@@ -24,13 +21,4 @@ else
 	DOCKER_OPTS=""
 fi
 
-#Executing proper command
-if [ "$EXEC_TYPE" == "docker" ]
-then
-	docker $DOCKER_OPTS "$@"
-elif [ "$EXEC_TYPE" == "compose" ]
-then
-	docker-compose $DOCKER_OPTS "$@"
-else
-	echo "Exec '$EXEC_TYPE' is unknown"
-fi
+_docker $DOCKER_OPTS "$@"
